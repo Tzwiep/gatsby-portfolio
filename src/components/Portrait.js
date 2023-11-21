1abf2884df906f744383183bff0f24a393d8dd6f
Tzwiep <55808390+Tzwiep@users.noreply.github.com>
2023-11-21 13:05:41 -0500

    Dark Mode

--- src/components/Portrait.js
+++ src/components/Portrait.js
@@ -2,9 +2,9 @@ import { StaticImage } from "gatsby-plugin-image"
 import React from 'react'
 
 export function Portrait() {
-    return <>
-<StaticImage src="../images/squareTZ-dark.jpg" alt="Tyler in dark mode" className="hidden mt-8 mx-0 xl:mx-8 xl:mt-4 dark:block" />
-<StaticImage src="../images/squareTZ.png" alt="Tyler" className="mt-8 mx-0 xl:mx-8 xl:mt-4 dark:hidden" />
-    </> 
+   return <>
+      <StaticImage src="../images/squareTZ-dark.jpg" alt="Tyler in Dark Mode" className="hidden mt-8 mx-0 xl:mx-8 xl:mt-4 dark:block" />
+   <StaticImage src="../images/squareTZ.png" alt="Tyler" className="mt-8 mx-0 xl:mx-8 xl:mt-4 dark:hidden" />
+</>
    }
    
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  purge: ['./src/**/*.{js,jsx,ts,tsx}'],
   darkMode: 'class',
   theme: {
     extend: {
