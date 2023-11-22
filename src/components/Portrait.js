af999de82f9b79781a15751c77bcb31a82bdeb0e
Tzwiep <55808390+Tzwiep@users.noreply.github.com>
2023-11-21 19:32:20 -0500

    Updated dark mode portrait
    changes to portrait.js

--- src/components/Portrait.js
+++ src/components/Portrait.js
@@ -1,10 +1,37 @@
-import { StaticImage } from "gatsby-plugin-image"
-import React from 'react'
 
-export function Portrait() {
-   return <>
-      <StaticImage src="../images/squareTZ-dark.jpg" alt="Tyler in Dark Mode" className="hidden mt-8 mx-0 xl:mx-8 xl:mt-4 dark:block" />
-   <StaticImage src="../images/squareTZ.png" alt="Tyler" className="mt-8 mx-0 xl:mx-8 xl:mt-4 dark:hidden" />
-</>
+   
+   import React, { useState, useEffect } from 'react';
+   import { StaticImage } from 'gatsby-plugin-image';
+   
+   export function Portrait() {
+       const [isDarkMode, setIsDarkMode] = useState(false);
+   
+       useEffect(() => {
+           // Check if the 'dark' class is applied to the body
+           const checkDarkMode = () => {
+               setIsDarkMode(document.body.classList.contains('dark'));
+           };
+   
+           // Set the initial state
+           checkDarkMode();
+   
+           // Add an event listener for class changes on the body
+           // This can be replaced with your dark mode toggle logic
+           const observer = new MutationObserver(checkDarkMode);
+           observer.observe(document.body, { attributes: true, attributeFilter: ['class'] });
+   
+           // Clean up the observer on component unmount
+           return () => observer.disconnect();
+       }, []);
+   
+       return (
+           <div>
+               {isDarkMode ? (
+                   <StaticImage src="../images/squareTZ-dark.jpg" alt="Tyler in Dark Mode" className="mt-8 mx-0 xl:mx-8 xl:mt-4" />
+               ) : (
+                   <StaticImage src="../images/squareTZ.png" alt="Tyler" className="mt-8 mx-0 xl:mx-8 xl:mt-4" />
+               )}
+           </div>
+       );
    }
    
